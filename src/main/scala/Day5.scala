d5ff43812638d27e70f4ecb9261eafbc605c9879
Benoit <bpasquereau@gmail.com>
2019-12-12 20:56:33 +0000

    Day 5 fixed bug in opcode 4

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -11,6 +11,7 @@ object Day5 {
         println("output="+output)
     }
 
+
     def run(p:Array[Int], input:List[Int]) = {
 
         var i = 0
@@ -19,7 +20,8 @@ object Day5 {
         var output = ListBuffer[Int]()
         while(p(i) % 100 != 99) {
             val opcode = p(i)
-            println(i+"\t"+opcode)
+            print(s"$i\t$opcode\t")
+
             def getParam(index:Int) = {
                 val mask = Map(1 -> 100, 2 -> 1000)
                 if ((opcode / mask(index)) % 10 > 0) 
@@ -27,15 +29,31 @@ object Day5 {
                 else
                     p(p(i+index))
             }
-    
+
+            def unaryOp(f : (Int) => Unit) = {
+                val p1 = getParam(1)
+                print(s"${p(i+1)}\t($p1)")
+                f(p1)
+                2
+            }
+
+            def binaryOp(f : (Int,Int) => Int) = {
+                val p1 = getParam(1)
+                val p2 = getParam(2)
+                print(s"${p(i+1)}\t${p(i+2)}\t${p(i+3)}\t($p1,$p2)")
+                p.update(p(i+3), f(p1, p2))
+                4
+            }
+
             val inc = opcode % 100 match {
-                case 1 => p.update(p(i+3),getParam(1)+getParam(2)) ; 4
-                case 2 => p.update(p(i+3),getParam(1)*getParam(2)) ; 4
-                case 3 => p.update(p(i+1), input(inputIndex)) ; inputIndex = inputIndex + 1 ; 2
-                case 4 => output += p(p(i+1)) ; 2
+                case 1 => binaryOp(_ + _)
+                case 2 => binaryOp(_ * _)
+                case 3 => print(input(inputIndex)) ; p.update(p(i+1), input(inputIndex)) ; inputIndex = inputIndex + 1 ; 2
+                case 4 => unaryOp( output += _ )
                 case o => throw new Exception("Unknown opcode "+o+" position "+i)
             }
             i = i + inc
+            println
         }
         output
 
